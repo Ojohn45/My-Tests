cbd28206ddc47f6b822c994405678a348966f2d3
jodumu <brucenic12@gmail.com>
2025-11-10 12:59:53 +0100

    my files

--- isnegative.go
+++ isnegative.go
@@ -1,17 +1,14 @@
-package main
+package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
-func IsNegative(n int) {
-	if n <= 0 {
+func IsNegative(nb int) {
+	if nb < 0 {
 		z01.PrintRune('T')
 	} else {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
-}
-
-func main() {
-	IsNegative(1)
-	IsNegative(-4)
 }
